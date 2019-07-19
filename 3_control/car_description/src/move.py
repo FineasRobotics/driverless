e5f6438d0bbd1786f3818c2b0200c2fdd11aaff6
Christos Avramis <it174918@it.teithe.gr>
2019-07-19 16:06:24 +0000

    Update move.py ,move.py is the default controller

--- 3_control/car_description/src/move.py
+++ 3_control/car_description/src/move.py
@@ -1,52 +1,63 @@
 #!/usr/bin/env python
 import rospy
+import os
 from geometry_msgs.msg import Twist
+from std_msgs.msg      import Float64
+
+rospy.init_node('controller', anonymous=True)
+pubBR = rospy.Publisher('/car/BackWheel_right_velocity_controller/command', Float64, queue_size=10)
+pubBL = rospy.Publisher('/car/BackWheel_left_velocity_controller/command', Float64, queue_size=10)
+pubFR = rospy.Publisher('/car/Turner_right_position_controller/command', Float64, queue_size=10)
+pubFL = rospy.Publisher('/car/Turner_left_position_controller/command', Float64, queue_size=10)
+
+def callback(data):
+    movementspeed = Float64()
+    angleRight = Float64()
+    angleLeft = Float64()
+    rospy.loginfo(rospy.get_caller_id() + "I heard x: %s y: %s", data.linear.x,data.angular.z)
+    movementspeed.data = data.linear.x
+    pubBR.publish(movementspeed)
+    pubBL.publish(movementspeed)
+    if data.angular.z != 0:
+    	y=data.angular.z 
+	if data.angular.z > 0.2:
+		y=0.2
+        if data.angular.z>0:
+		y1=y+0.05
+		y2=y
+	elif data.angular.z<0:
+		y1=y
+		y2=y+0.05
+	else:
+		y1=0.00001
+		y2=0.00001
+	angleRight.data = y1
+        angleLeft.data = y2
+	#Z IS REVERSED 
+	pubFR.publish(angleLeft)
+    	pubFL.publish(angleRight)
+
 
 def move():
-    # Starts a new node
-    rospy.init_node('robot_cleaner', anonymous=True)
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    vel_msg = Twist()
-    
-    #Receiveing the user's input
-    print("Let's move your robot")
-    speed = input("Input your speed:")
-    distance = input("Type your distance:")
-    isForward = input("Foward?: ")#True or False
-    
-    #Checking if the movement is forward or backwards
-    if(isForward):
-        vel_msg.linear.x = abs(speed)
-    else:
-        vel_msg.linear.x = -abs(speed)
-    #Since we are moving just in x-axis
-    vel_msg.linear.y = 0
-    vel_msg.linear.z = 0
-    vel_msg.angular.x = 0
-    vel_msg.angular.y = 0
-    vel_msg.angular.z = 0
-    
-    while not rospy.is_shutdown():
 
-        #Setting the current time for distance calculus
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
-
-        #Loop to move the turtle in an specified distance
-        while(current_distance < distance):
-            #Publish the velocity
-            velocity_publisher.publish(vel_msg)
-            #Takes actual time to velocity calculus
-            t1=rospy.Time.now().to_sec()
-            #Calculates distancePoseStamped
-            current_distance= speed*(t1-t0)
-        #After the loop, stops the robot
-        vel_msg.linear.x = 0
-        #Force the robot to stop
-        velocity_publisher.publish(vel_msg)
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.Subscriber("/car/cmd_vel", Twist, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
         #Testing our function
         move()
     except rospy.ROSInterruptException: pass
+
+
+
+
+
+
+
