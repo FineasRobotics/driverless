2a63ab6f47f2cda151c95e6f58fdf6422480d983
SimonPlat <S.platiotis@gmail.com>
2019-08-08 14:32:02 +0300

    Quick fix on tf_robot_odom_bridge.py.

--- 3_control/car_description/src/tf_robot_odom_bridge.py
+++ 3_control/car_description/src/tf_robot_odom_bridge.py
@@ -10,6 +10,11 @@ from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 rospy.init_node('tf_publisher') # initialize node
 br = tf.TransformBroadcaster() # broadcasts tf msgs for base_footprint - odom
 r = rospy.Rate(10.0)
+# Band-aid untill the exception is fixxed.
+r.sleep()
+r.sleep()
+r.sleep()
+# 
 br.sendTransform((0.0, 0.0, 0.0),
                          (0.0, 0.0, 0.0, 1.0),
                          rospy.Time.now(),
@@ -18,15 +23,17 @@ br.sendTransform((0.0, 0.0, 0.0),
 while not rospy.is_shutdown():
    # first, we'll get the pose from gazebo api
    resp_coordinates = 0
+   # !!Exception needs to be handled properly, nodes must be robust to different launching orders!!
    try:
 	model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state',GetModelState)
 	resp_coordinates = model_coordinates("car","")
 	x = resp_coordinates.pose.position.x
    except rospy.ServiceException as e:
-        rospy.loginfo("Get model State service call failed:".format(e))
-        break #if rosservice fails abort - it can store last data and publish them instead of aborting ??
-   #print "get some tf as proof of connectiong between map and robot " + str(x)
-   # first, we'll publish the transform over tf
+        rospy.loginfo("Get model State service call failed: {0}".format(e))
+        break 
+          #if rosservice fails abort - it can store last data and publish them instead of aborting ??
+          #print "get some tf as proof of connectiong between map and robot " + str(x)
+          # first, we'll publish the transform over tf
    br.sendTransform(
         (resp_coordinates.pose.position.x,resp_coordinates.pose.position.y,0),
         (resp_coordinates.pose.orientation.x ,resp_coordinates.pose.orientation.y ,resp_coordinates.pose.orientation.z ,resp_coordinates.pose.orientation.w),
@@ -34,6 +41,5 @@ while not rospy.is_shutdown():
         "base_footprint",
         "map"
    )
-   
    r.sleep()
 
